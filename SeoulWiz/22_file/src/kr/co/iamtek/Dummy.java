a1e0c1dab75dba6348d67adcf01559cf5a7179b7
blitz-out <blitz70@hanmail.net>
2016-08-20 15:25:36 +0900

    JSP seoul wiz, src error update
    added dummy package and servlet

---
+++ SeoulWiz/22_file/src/kr/co/iamtek/Dummy.java
@@ -0,0 +1,24 @@
+package kr.co.iamtek;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/dummy")
+public class Dummy extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("doGet");
+		PrintWriter out = resp.getWriter();
+		out.println("dummy servlet");
+	}
+	
+}
